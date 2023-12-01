b84c20239135a3566475c47613906e24e03614cc
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-12-01 13:14:00 +0530

    updated

--- prime-video-plus-react/src/pages/ProductsPage/ProductsPage.js
+++ prime-video-plus-react/src/pages/ProductsPage/ProductsPage.js
@@ -1,7 +1,25 @@
 const ProductsPage = () => {
+  // API Url: https://fakestoreapi.com/products
+
   return (
-    <div>ProductsPage</div>
-  )
-}
+    <div className="row">
+      <h1>Shop our Products</h1>
+
+      <div className="col-md-3">
+        <div className="card">
+          <img src="" className="card-img-top" alt="" height="300" />
+          <div className="card-body">
+            <h5 className="card-title">Bread | Category</h5>
+            <p className="card-text">description</p>
+            <p className="card-text">price $ 100</p>
+            <button type="button" className="btn btn-primary">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductsPage;
