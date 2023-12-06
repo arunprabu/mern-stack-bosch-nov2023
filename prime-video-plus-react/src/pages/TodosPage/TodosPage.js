1167eed386b4e57ec45e20db4769fa3a736c085a
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-12-06 12:28:39 +0530

    updated

--- prime-video-plus-react/src/pages/TodosPage/TodosPage.js
+++ prime-video-plus-react/src/pages/TodosPage/TodosPage.js
@@ -1,7 +1,59 @@
+import { useEffect, useReducer, useRef } from "react";
+import todoReducer from "../../reducers/todoReducer";
+
 const TodosPage = () => {
+  // Let's try to collect the todo input form elements's data in uncontrolled component's way
+  const todoInputRef = useRef();
+
+  const [todoState, todoDispatch] = useReducer(todoReducer);
+  console.log(todoState); // undefined at first
+  console.log(todoDispatch); // is a fn
+
+  useEffect(() => {
+    // Let's load the todos after initial rendering
+    todoDispatch({
+      type: "LIST_TODOS",
+    });
+  }, []);
+
+  const handleAddTodo = () => {
+    console.log(todoInputRef.current.value);
+    const todoItem = {
+      id: new Date().toISOString(),
+      title: todoInputRef.current.value,
+      isCompleted: false,
+    };
+    console.log(todoItem); // this todo should be added into an array of todos
+    // Let's dispatch an action to add a todo
+    todoDispatch({
+      type: "ADD_TODO",
+      payload: todoItem,
+    });
+  };
+
   return (
-    <div>TodosPage</div>
-  )
-}
+    <div>
+      <h1>Todos App! | useRef and useReducer Hooks</h1>
+      <input type="text" placeholder="Enter Todo" ref={todoInputRef} />
+      <button onClick={handleAddTodo}>Add TODO</button>
+      <hr />
+
+      <div>
+        <h2>Listing Todos</h2>
+        {todoState?.length === 0 && (
+          <div className="alert alert-warning">
+            No Todos Found. You can add one
+          </div>
+        )}
+
+        <ul>
+          {todoState?.map((todo) => {
+            return <li key={todo.id}>{todo.title}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default TodosPage;
---
+++ prime-video-plus-react/src/reducers/todoReducer.js
@@ -0,0 +1,30 @@
+/*
+Reducer?
+----
+  * a function that takes in state and action as params
+  * it must return state 
+  * action is an object. it must have type property and an optional payload property
+  * action types are the possible actions that will occur in the feature
+  * example action types are 'ADD_TODO' and 'LIST_TODOS', 'EDIT_TODO', 'DELETE_TODO'
+  * action can also have payload property
+  * the reducer function will never be called directly from component 
+  * There's only one way to call the reducer fn.. THE component must dispatch an action
+*/
+
+function todoReducer(state = [], action) {
+  console.log(action);
+  console.log(state); // state logic -- array of object for maintaining list of todos
+
+  switch (action.type) {
+    case "LIST_TODOS":
+      return state;
+
+    case "ADD_TODO":
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+}
+
+export default todoReducer;
