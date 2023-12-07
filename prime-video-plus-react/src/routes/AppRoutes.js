7a19d7be54b0ef8a4f887238e6f8be963a7e10f2
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-12-07 12:11:52 +0530

    updated

--- prime-video-plus-react/src/routes/AppRoutes.js
+++ prime-video-plus-react/src/routes/AppRoutes.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import PrimeVideoPage from "../pages/PrimeVideoPage/PrimeVideoPage";
-import UsersPage from "../pages/UsersPage/UsersPage";
+// the following comps are NOT lazily loaded
+import HomePage from "../pages/HomePage/HomePage";
 import ProductsPage from "../pages/ProductsPage/ProductsPage";
 import TodosPage from "../pages/TodosPage/TodosPage";
 import UnitTestingDemoPage from "../pages/UnitTestingDemoPage/UnitTestingDemoPage";
@@ -9,33 +9,40 @@ import AboutUsPage from "../pages/AboutUsPage/AboutUsPage";
 import ContactUsPage from "../pages/ContactUsPage/ContactUsPage";
 import AddUser from "../pages/UsersPage/components/AddUser";
 import UserDetails from "../pages/UsersPage/components/UserDetails";
-import HomePage from "../pages/HomePage/HomePage";
 import { ErrorBoundary } from "react-error-boundary";
+
+// the following components are lazily loaded
+const PrimeVideoPage = lazy(() =>
+  import("../pages/PrimeVideoPage/PrimeVideoPage")
+);
+const UsersPage = lazy(() => import("../pages/UsersPage/UsersPage"));
 
 const AppRoutes = () => {
   return (
-    <ErrorBoundary
-      fallback={
-        <div className="alert alert-danger">
-          Some Error occured. Try again later!
-        </div>
-      }
-    >
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/prime-video" element={<PrimeVideoPage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/users/add" element={<AddUser />} />
-        {/* Let's work with URL param (userId) -- for dynamic urls */}
-        <Route path="/users/:userId" element={<UserDetails />} />
-        {/* <Route path="/users/:userId/edit" element={<EditUser />} /> */}
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/todos" element={<TodosPage />} />
-        <Route path="/unit-testing-demo" element={<UnitTestingDemoPage />} />
-        <Route path="/about-us" element={<AboutUsPage />} />
-        <Route path="/contact-us" element={<ContactUsPage />} />
-      </Routes>
-    </ErrorBoundary>
+    <Suspense fallback={<div className="spinner spinner-border"></div>}>
+      <ErrorBoundary
+        fallback={
+          <div className="alert alert-danger">
+            Some Error occured. Try again later!
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/prime-video" element={<PrimeVideoPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/add" element={<AddUser />} />
+          {/* Let's work with URL param (userId) -- for dynamic urls */}
+          <Route path="/users/:userId" element={<UserDetails />} />
+          {/* <Route path="/users/:userId/edit" element={<EditUser />} /> */}
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/todos" element={<TodosPage />} />
+          <Route path="/unit-testing-demo" element={<UnitTestingDemoPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+        </Routes>
+      </ErrorBoundary>
+    </Suspense>
   );
 };
 
