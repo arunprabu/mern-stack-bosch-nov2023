250f65c8e28aa2c365acc6bfe9168b0f3379753f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-12-20 11:23:56 +0530

    updated

--- socketio-webrtc-apps/app.js
+++ socketio-webrtc-apps/app.js
@@ -23,11 +23,12 @@ app.get("/video-chat", (req, res) => {
 
 // listen on socket connections
 io.on('connection', (socket) => {
+  /* text chat logic starts */
   // The following console log will be visible in server side console
-  console.log('A New User Connected');
+  console.log("A New User Connected");
 
   // you can listen on all emitted events from front end socket.io client
-  socket.on('open_chat_session', (message) => {
+  socket.on("open_chat_session", (message) => {
     // The following console log will be visible in server side console
     console.log(message); // msg received from socket.io client
     // Let's send the message to all socket.io clients
@@ -38,7 +39,26 @@ io.on('connection', (socket) => {
     // sending the updates to all clients
     io.emit("server_typing", info);
   });
+  /* text chat logic ends */
 
+  /* video chat logic starts  -- using WebRTC */
+  let otherUser;
+  // receiving signal in socket server
+  socket.on('offer', (offer) => {
+    console.log(offer);
+    otherUser = socket.id;
+    socket.to(otherUser).emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    console.log(answer);
+    socket.to(otherUser).emit("answer", answer);
+  });
+
+  socket.on('ice_candidate', (candidate) => {
+    socket.to(otherUser).emit("ice_candidate", candidate);
+  });
+  /* video chat logic ends */
 });
 
 // the following should be server.listen -- NOT app.listen
